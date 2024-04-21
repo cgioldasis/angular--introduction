51da147f2249e45e6e0044aa7647a6d2cd143e78
Christos Gioldasis <chris.gioldasis@gmail.com>
2024-04-21 14:06:26 +0300

    user registration not ready yet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // apiURL: 'https://coding-factory-api.ddns.net'
-    apiURL: 'http://localhost:5000'
+     apiURL: 'https://coding-factory-api.ddns.net'
 };
